664eecefadfb0be5d4c9cd9f3443ab6eab3912c8
Bogdan Vera <bogdan.vera@eecs.qmul.ac.uk>
2013-02-13 15:06:17 +0000

    Removed unnecessary use of dynamicmatch toolbox.

--- notealign2_usespastogram.m
+++ notealign2_usespastogram.m
@@ -73,49 +73,42 @@ end
 
 %%
 
-%%pianoroll(ch1_p, 'num', 'sec')
 
-% ITT we hack the miditoolbox notation matcher and fill the M structure
-% with our own data.
-
-% first do it with DTW
-MATCH_DTW = dynamicmatch(ch1_p,notation_1,'prelude');
 
 %%
 
-%now replace the match matrix with the pastogram data
-%have to-reformat it so it's 1-to-1
+%now fil the match matrix with the pastogram data
+%
 
 %empty it
-MATCH_DTW2.M = [];
+M = [];
 %FORMAT: NOTATION INDEX, PERF INDEX, NOTATION NOTE, PERF NOTE, NOTATION
 %ONSET, PERF ONSET, NOT. VEL, PERF. VEL, NOT. DURATION, PERF. DURATION
 
 for i = 1:length(ch1_p)
 
-    MATCH_DTW2.M(i,1) = i;
-    MATCH_DTW2.M(i,2) = solutions(i);
+   M(i,1) = i;
+    M(i,2) = solutions(i);
     
-     MATCH_DTW2.M(i,3) = notation_1(i,4);
-      MATCH_DTW2.M(i,4) = ch1_p(solutions(i),4);
+     M(i,3) = notation_1(i,4);
+      M(i,4) = ch1_p(solutions(i),4);
       
-       MATCH_DTW2.M(i,5) = notation_1(i,6);
-        MATCH_DTW2.M(i,6) =  ch1_p(i,6);
+       M(i,5) = notation_1(i,6);
+        M(i,6) =  ch1_p(i,6);
         
-        MATCH_DTW2.M(i,7) = notation_1(i,5);
-        MATCH_DTW2.M(i,8) =  ch1_p(i,5);
+        M(i,7) = notation_1(i,5);
+        M(i,8) =  ch1_p(i,5);
         
-          MATCH_DTW2.M(i,9) = notation_1(i,7);
-        MATCH_DTW2.M(i,10) =  ch1_p(i,7);
+          M(i,9) = notation_1(i,7);
+        M(i,10) =  ch1_p(i,7);
 
 end
 
 %%
-% now replace the M matrix into the match
-MATCH_DTW.M = MATCH_DTW2.M;
+
 %%
 %%
-gmw('create',MATCH_DTW)
+
 
 %%
 
@@ -125,7 +118,7 @@ test = {'M', 100,20,30};
 test{2,3} = 100;
 
 %% EXPORTS CSV OF THE MATCH
-xlswrite('notematch.xls', MATCH_DTW2.M, 'notes', 'notes');
+xlswrite('notematch.xls', M, 'notes', 'notes');
 %% EXPORTS CSV OF MIDI DATA
 
 
@@ -134,6 +127,3 @@ xlswrite('notation.xls', notation_1, 'notes', 'notes');
 xlswrite('performance.xls', ch1_p, 'notes', 'notes');
 
 %%
-colormap(jet);
-imagesc(performance.histoNB);
-title('hg');
